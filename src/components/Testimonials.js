dbed68355f74e8befe06bd37711491593e434bf2
Pratik-Yabaji <paratikyabaji@gmail.com>
2022-07-24 20:03:19 +0530

    Week_02 Assaignment

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,13 +3,7 @@ import React from "react";
 export default function Testimonials() {
   return (
     <>
-    <svg  class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%">
-    </rect></svg>
-
-        <img src="https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg" alt=""className='backgroundPill' 
-        style={{'background':'black'}}
-        width={100} height={100}/>
-      {/* <div className="container my-5">
+      <div className="container my-5">
         <div className="jumbotron bg-secondary rounded p-5 text-white">
           <h1 className="display-4">Hi !!</h1>
           <p className="lead">
@@ -22,7 +16,7 @@ export default function Testimonials() {
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, quas.
           </p>
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
